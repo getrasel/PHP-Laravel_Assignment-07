d512327934704fd9b4497347216c1b065ab5d2c0
Rasel <mdraselahmed063@gmail.com>
2023-11-15 20:53:06 +0600

    Assignment 7

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function Hello(){
+        return "Hello, Laravel!";
+    }
+    function assignment7(){
+        return "this is assignment 7";
+    }
+}
